84b5bbc41c66ef6b256e3aead3bcb84effda93cb
Liu Qun <517067180@qq.com>
2019-04-12 19:50:34 +0800

    精简内核模块代码
    删除了以下几个子函数, 将逻辑处理集中到上一级主程序内.
    - demo_prepare_reply()
    - demo_mk_reply()
    - demo_send_reply()

--- demo-2/kernel-module/main.c
+++ demo-2/kernel-module/main.c
@@ -23,61 +23,13 @@ static const struct nla_policy demo_cmd_policy[DEMO_CMD_ATTR_MAX+1] = {
     [DEMO_CMD_ATTR_DATA] = { .type = NLA_S32 },
 };
 
-static int demo_prepare_reply(struct genl_info *info, u8 cmd, struct sk_buff **skbp, size_t size)
-{
-    struct sk_buff *skb;
-    void *reply;
-
-    /*
-     * If new attributes are added, please revisit this allocation
-     */
-    skb = genlmsg_new(size, GFP_KERNEL);
-    if (!skb) {
-        return -ENOMEM;
-    }
-
-    if (!info) {
-        return -EINVAL;
-    }
-
-    /* 构建回发消息头 */
-    reply = genlmsg_put_reply(skb, info, &demo_family, 0, cmd);
-    if (reply == NULL) {
-        nlmsg_free(skb);
-        return -EINVAL;
-    }
-
-    *skbp = skb;
-    return 0;
-}
-
-static int demo_mk_reply(struct sk_buff *skb, int aggr, void *data, int len)
-{
-    /* add a netlink attribute to a socket buffer */
-    return nla_put(skb, aggr, len, data);
-
-    /*
-     * nla是可以分level的(调用nla_nest_start和nla_nest_end进行)，
-     * 由于本示例程序attr较少，所以就不进行分层了，分层具体可见
-     * 内核中其他使用到genetlink的地方。
-     */
-}
-
-static int demo_send_reply(struct sk_buff *skb, struct genl_info *info)
-{
-    struct genlmsghdr *genlhdr = nlmsg_data(nlmsg_hdr(skb));
-    void *reply = genlmsg_data(genlhdr);
-
-    genlmsg_end(skb, reply);
-
-    return genlmsg_reply(skb, info);
-}
-
 static int cmd_attr_echo_message(struct genl_info *info)
 {
     struct nlattr *na;
     char *msg;
+    size_t nbytes; // 包括额外1字节字符串结束符
     struct sk_buff *rep_skb;
+    void *begin;
     size_t size;
     int ret;
 
@@ -91,25 +43,35 @@ static int cmd_attr_echo_message(struct genl_info *info)
     pr_info("demo generic netlink receive echo mesg %s\n", msg);
 
     /* 回发消息 */
-    size = nla_total_size(strlen(msg) + 1);
-
-    /* 准备构建消息 */
-    ret = demo_prepare_reply(info, DEMO_CMD_REPLY, &rep_skb, size);
-    if (ret < 0) {
-        return ret;
+    /* 1. 申请新sk_buff */
+    nbytes = strlen(msg) + 1;
+    size = nla_total_size(nbytes);
+    rep_skb = genlmsg_new(size, GFP_KERNEL);
+    if (!rep_skb) {
+        return -ENOMEM;
     }
-
-    /* 填充消息 */
-    ret = demo_mk_reply(rep_skb, DEMO_CMD_ATTR_MESG, msg, size);
+    /* 2. 开始填充genetlink消息具体内容 */
+    begin = genlmsg_put_reply(rep_skb, info, &demo_family, 0, DEMO_CMD_REPLY);
+    if (!begin) {
+        pr_info("Error: genlmsg_put_reply() failed\n");
+        ret = -ENOMEM;
+        goto err;
+    }
+    ret = nla_put(rep_skb, DEMO_CMD_ATTR_MESG, nbytes, msg);
     if (ret < 0) {
         goto err;
     }
-
-    /* 完成构建并发送 */
-    return demo_send_reply(rep_skb, info);
+    /* 3. 内容填充完毕 */
+    genlmsg_end(rep_skb, begin);
+    /* 4. 发送sk_buff */
+    ret = genlmsg_reply(rep_skb, info);
+    if (ret < 0) {
+        goto err;
+    }
+    return ret;
 
 err:
-    nlmsg_free(rep_skb);
+    kfree_skb(rep_skb);
     return ret;
 }
 
@@ -118,6 +80,7 @@ static int cmd_attr_echo_data(struct genl_info *info)
     struct nlattr *na;
     s32 data;
     struct sk_buff *rep_skb;
+    void *begin;
     size_t size;
     int ret;
 
@@ -131,22 +94,34 @@ static int cmd_attr_echo_data(struct genl_info *info)
     pr_info("demo generic netlink receive echo data %d\n", data);
 
     /* 回发数据 */
+    /* 1. 申请新sk_buff */
     size = nla_total_size(sizeof(s32));
-
-    ret = demo_prepare_reply(info, DEMO_CMD_REPLY, &rep_skb, size);
-    if (ret < 0)
-        return ret;
-
-    /* 为了简单这里直接调用netlink库函数(对于需求的丰富可以自行封装) */
+    rep_skb = genlmsg_new(size, GFP_KERNEL);
+    if (!rep_skb) {
+        return -ENOMEM;
+    }
+    /* 2. 开始填充genetlink消息具体内容 */
+    begin = genlmsg_put_reply(rep_skb, info, &demo_family, 0, DEMO_CMD_REPLY);
+    if (!begin) {
+        pr_info("Error: genlmsg_put_reply() failed\n");
+        ret = -ENOMEM;
+        goto err;
+    }
     ret = nla_put_s32(rep_skb, DEMO_CMD_ATTR_DATA, data);
     if (ret < 0) {
         goto err;
     }
-
-    return demo_send_reply(rep_skb, info);
+    /* 3. 内容填充完毕 */
+    genlmsg_end(rep_skb, begin);
+    /* 4. 发送sk_buff */
+    ret = genlmsg_reply(rep_skb, info);
+    if (ret < 0) {
+        goto err;
+    }
+    return ret;
 
 err:
-    nlmsg_free(rep_skb);
+    kfree_skb(rep_skb);
     return ret;
 }
 
